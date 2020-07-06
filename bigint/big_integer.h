604d64ba91dcfc9bda0115acc57b435926f9cb6a
Emil <emil2001garipov@gmail.com>
2020-07-06 17:31:23 +0300

    bigint

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -1,16 +1,22 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
 
-#include <cstddef>
-#include <gmp.h>
-#include <iosfwd>
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
 
 struct big_integer
 {
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
+    big_integer(long long a);
     explicit big_integer(std::string const& str);
+
+    big_integer(uint32_t a);
+
     ~big_integer();
 
     big_integer& operator=(big_integer const& other);
@@ -45,10 +51,24 @@ struct big_integer
     friend bool operator<=(big_integer const& a, big_integer const& b);
     friend bool operator>=(big_integer const& a, big_integer const& b);
 
+    uint32_t operator[](size_t index) const;
+    void swap(big_integer& b);
     friend std::string to_string(big_integer const& a);
 
-private:
-    mpz_t mpz;
+    // :TODO: make it private
+    std::vector <uint32_t> data_;
+//private:
+    bool sign;
+    size_t sz_;
+    void shrink();
+    void expand(size_t len);
+    bool compare_prefix(const big_integer &a, const big_integer &b, size_t pos, size_t m);
+    uint32_t calc3to2(const big_integer &a, const big_integer &b, size_t i, size_t m);
+    big_integer& div_uint(uint32_t const b);
+    big_integer& mul_uint(uint32_t const rhs);
+    uint32_t first();
+
+    big_integer &difference(const big_integer &b, size_t offset, size_t m);
 };
 
 big_integer operator+(big_integer a, big_integer const& b);
@@ -73,5 +93,7 @@ bool operator>=(big_integer const& a, big_integer const& b);
 
 std::string to_string(big_integer const& a);
 std::ostream& operator<<(std::ostream& s, big_integer const& a);
+uint32_t current_digit(const big_integer &a, const big_integer &b, size_t i);
+bool compare_prefix(const big_integer &a, const big_integer &b, size_t pos);
 
 #endif // BIG_INTEGER_H
